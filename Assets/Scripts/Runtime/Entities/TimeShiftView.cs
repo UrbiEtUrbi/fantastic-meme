78fec6ccc639a78ba985715ace32b0853dcea3e3
Urban Strajnar <urban.strajnar@trickytribe.com>
2024-05-24 21:39:51 +0200

    fix a layer bug

--- Assets/Scripts/Runtime/Entities/TimeShiftView.cs
+++ Assets/Scripts/Runtime/Entities/TimeShiftView.cs
@@ -83,6 +83,7 @@ public class TimeShiftView : MonoBehaviour
             return;
         }
         SoundManager.Instance.Play("timepower_deactivate", ControllerGame.Player.transform);
+        ControllerGame.TimeManager.UnShiftEnemies();
         if (timer <= TimeToShow / 2f)
         {
             ControllerGame.TimeManager.ShiftEnemies();
